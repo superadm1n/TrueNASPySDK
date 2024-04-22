1f2a288a238c5c0e7c98d3d7531e0a1385f52912
Kyle Kowalczyk <kowalkyl@gmail.com>
2024-04-21 22:31:48 -0500

    Refactored URL rewrite to a decorator

--- RestAPI/Base.py
+++ RestAPI/Base.py
@@ -1,15 +1,15 @@
 from requests import Session
 
 
-class TrueNASBase(Session):
+def construct_url(func):
+    """
+    Decorator that will rewrite the URL to query on the TrueNAS server and prepend the base URL
+    provide to the TrueNASBase object to any URL provided.
+    :param func:
+    :return:
+    """
 
-    def __init__(self, api_key, host, api_version='2.0', verify=False, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.verify = verify
-        self.headers = {'Authorization': f'Bearer {api_key}'}
-        self.base_url = f'https://{host}/api/v{api_version}'
-
-    def _construct_url(self, *args, **kwargs):
+    def wrapper_func(self, *args, **kwargs):
         if kwargs.get('url'):
             uri = kwargs.get('url')
             kwargs['url'] = self.base_url + uri
@@ -19,24 +19,31 @@ class TrueNASBase(Session):
             tmp[0] = self.base_url + uri
             args = tuple(tmp)
 
-        return args, kwargs
+        return func(self, *args, **kwargs)
 
+    return wrapper_func
+
+
+class TrueNASBase(Session):
+
+    def __init__(self, api_key, host, api_version='2.0', verify=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.verify = verify
+        self.headers = {'Authorization': f'Bearer {api_key}'}
+        self.base_url = f'https://{host}/api/v{api_version}'
+
+    @construct_url
     def get(self, *args, **kwargs):
-        args, kwargs = self._construct_url(*args, **kwargs)
         return super().get(*args, **kwargs)
 
+    @construct_url
     def put(self, *args, **kwargs):
-        args, kwargs = self._construct_url(*args, **kwargs)
         return super().get(*args, **kwargs)
 
+    @construct_url
     def delete(self, *args, **kwargs):
-        args, kwargs = self._construct_url(*args, **kwargs)
         return super().delete(*args, **kwargs)
 
+    @construct_url
     def post(self, *args, **kwargs):
-        args, kwargs = self._construct_url(*args, **kwargs)
         return super().post(*args, **kwargs)
-
-
-
-
